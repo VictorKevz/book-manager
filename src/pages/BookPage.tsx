f5057fed6c29fe9795eb31f4fbf8bb4f590f7b2d
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-06-29 18:21:24 +0300

    Style BookCard

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -13,7 +13,7 @@ export const BookPage = () => {
     );
   return (
     <section className="max-w-screen-2xl w-full flex-col items-center justify-center ">
-      <div className="w-full grid grid-cols-3 gap-5 my-6">
+      <div className="w-full grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 my-6">
         {books.map((book) => (
           <BookCard key={book?.id} book={book} />
         ))}
